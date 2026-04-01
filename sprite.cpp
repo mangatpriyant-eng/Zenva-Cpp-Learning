9d8748ddf023bdc6dac692b6b20a52aa0f31a5fb
mangatpriyant-eng <mangatpriyant@gmail.com>
2026-04-01 17:28:21 +0000

    Add distanceFromOrigin method to sprite.cpp

--- sprite.cpp
+++ sprite.cpp
@@ -12,17 +12,22 @@ struct Sprite {
     void show() {
         visible = true;
     }
+
+    float distanceFromOrigin() {
+        return x + y;
+    }
 };
 
 int main() {
     Sprite s;
-    s.x = 10.0;
-    s.y = 5.0;
+    s.x = 6.0;
+    s.y = 7.0;
     s.visible = true;
 
     std::cout << s.visible << std::endl;
     s.hide();
     std::cout << s.visible << std::endl;
+    std::cout << s.distanceFromOrigin() << std::endl;
 
     return 0;
 }
