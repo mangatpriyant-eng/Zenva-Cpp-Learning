5f7a6d69449ecdef5063bc84d242bb5e513ef1c6
mangatpriyant-eng <mangatpriyant@gmail.com>
2026-04-05 16:33:31 +0000

    Update struct.cpp with comments

--- struct.cpp
+++ struct.cpp
@@ -1,25 +1,24 @@
-#include <iostream>
+#include <iostream> 
 
 struct Position {
     float x;
     float y;
+// constructor can be added to struct as well, but it's not required
+Position(float startX, float startY) { //why is it Position and not void i mean it not function position    
+x = startX;
+y = startY;
+}
 
-    Position(float startX, float startY) {
-        x = startX;
-        y = startY;
-    }
-
-    void print() {
-        std::cout << "X: " << x << " Y: " << y << std::endl;
-    }
+//Method Member functions can also be added to struct as well but it is not required
+void print() {
+    std::cout << "X: " << x << " Y: " << y << std::endl;
+}
 };
 
-int main() {
-    Position pos(3.0, 5.0);
-    pos.print();
-
-    pos.x = 10.0;
-    pos.print();
-
-    return 0;
+int main () {
+  Position pos(3.0 , 5.0); // constructor can be used to initialize the struct members
+   pos.print();
+   pos.x = 10.0 ; // members can be accessed and modified directly using the . operator
+   pos.print();
+    return 0; 
 }
