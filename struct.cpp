fd80c49e12dbb8646ad7738f39e475a604a7fb3c
mangatpriyant-eng <mangatpriyant@gmail.com>
2026-04-05 15:17:39 +0000

    yes

--- struct.cpp
+++ struct.cpp
@@ -5,13 +5,13 @@ struct Position {
     float y;
 // constructor can be added to struct as well, but it's not required
 Position(float startX, float startY) { //why is ir Position and not void i mean it not function position    
-};
+} //;
 
 //Method Member functions can also be added to struct as well but it is not required
 void print() {
     std::cout << "X: " << x << " Y: " << y << std::endl;
 }
-//}; //return0;();; // still directly accesible without needing to create an object of the struct and separete 
+}; //}; //return0;();; // still directly accesible without needing to create an object of the struct and separete 
 int main () {
   //  Position pos;
   Position pos(3.0 , 5.0); // constructor can be used to initialize the struct members
@@ -22,5 +22,5 @@ int main () {
    pos .x = 10.0 ; // members of the struct can be accessed and modified directly using the . operator
   //  std::cout <<"X: " << pos.x << " y: " << pos.y << std::endl;
    pos.print(); // method can be used to print the struct members after modification 
-    return 0; }
-//}
+    return 0; 
+}
