b32d15f2178b9d24fef0e89827dea0db95436e9d
mangatpriyant-eng <mangatpriyant@gmail.com>
2026-04-08 21:25:14 +0000

    update

--- class_backup.cpp
+++ class_backup.cpp
@@ -5,25 +5,13 @@ class Player {
     private: //;  // acess specifer private is used to make the member private
     std::string name;
     int health; 
-    // meh
+
     public:
     //constructor is a special member function 
     Player(std::string playerName, int playerHealth) {
         name = playerName;
-        health = playerHealth; }
-    // 
-    // Getter 
-    int getHealth() 
-    {
-        return health;
+        health = playerHealth;
     }
-
-    // Setter 
-    
-    void setHealth(int newHealth) {
-        health = newHealth;    
-    }    
-    //}
 
 // Method member function can be added to class
 void print() {
@@ -33,13 +21,7 @@ void print() {
 
 int main() {
     Player p("Mario", 100);
-
-    std::cout << "Player Health is " << p.getHealth()<< std::endl;
-    //p.getHealth;
-    p.setHealth(-18);
-    std::cout << "What a Strike!!! Player Health has reduced to 82" << p.getHealth() << std::endl;
     p.print();
-    //bvb
+
     return 0;
 }
-
